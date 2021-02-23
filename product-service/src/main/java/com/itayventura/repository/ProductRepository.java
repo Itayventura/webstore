1fd895efcde88aea4442a106be58eeeedb1c080c
itayventura <ventura.itay@gmail.com>
2021-02-23 17:08:02 +0200

    modifying product name, adding a description to swagger ui

--- product-service/src/main/java/com/itayventura/repository/ProductRepository.java
+++ product-service/src/main/java/com/itayventura/repository/ProductRepository.java
@@ -1,11 +1,13 @@
 package com.itayventura.repository;
 
 import com.itayventura.domain.Product;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+@Tag(name = "Product", description = "The Product API")
 @Repository
 public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
 

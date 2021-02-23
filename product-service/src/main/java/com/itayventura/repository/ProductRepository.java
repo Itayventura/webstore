3e4028de1df2af05e43a850c3ac7bec47eca5ddd
itayventura <ventura.itay@gmail.com>
2021-02-23 16:33:02 +0200

    modifying productsrepo to extend from pagingAndSorting

--- product-service/src/main/java/com/itayventura/repository/ProductRepository.java
+++ product-service/src/main/java/com/itayventura/repository/ProductRepository.java
@@ -1,10 +1,10 @@
 package com.itayventura.repository;
 
 import com.itayventura.domain.Product;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends CrudRepository<Product, Long>{
+public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
 
 }

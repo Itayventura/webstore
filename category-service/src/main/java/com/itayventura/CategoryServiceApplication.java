320d62ef4f1024c3941bce35349ea6a152cc0dae
itayventura <ventura.itay@gmail.com>
2021-02-23 19:29:23 +0200

    adding dependency to pom file, enable discovery client

--- category-service/src/main/java/com/itayventura/CategoryServiceApplication.java
+++ category-service/src/main/java/com/itayventura/CategoryServiceApplication.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CategoryServiceApplication implements CommandLineRunner {
 	private final CategoryRepository repository;
 

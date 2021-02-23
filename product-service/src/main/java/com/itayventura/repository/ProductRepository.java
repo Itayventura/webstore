f536c0d752d51eb700abadd834da391d8f4f9959
itayventura <ventura.itay@gmail.com>
2021-02-23 16:46:15 +0200

    adding a method serach products by category, returns a page

--- product-service/src/main/java/com/itayventura/repository/ProductRepository.java
+++ product-service/src/main/java/com/itayventura/repository/ProductRepository.java
@@ -1,10 +1,14 @@
 package com.itayventura.repository;
 
 import com.itayventura.domain.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
+
+    Page<Product> findByCategoryId(Long categoryId, Pageable pageable);
 
 }
